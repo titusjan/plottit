d35ad742de4ce203249ce665db4a0763f51c8351
Pepijn Kenter <titusjan@gmail.com>
2011-12-27 12:29:01 +0000

    Faster html decoding Added 1500 post to testData
    git-svn-id: file:///var/svn/programming/listit@146 1fc08600-0457-4e62-b340-16eb51c54387

--- src/chrome/content/utils.js
+++ src/chrome/content/utils.js
@@ -55,6 +55,18 @@ Listit.booleanToString = function(boolStr) {
     return boolStr ? 'true' : 'false';
 }
 
+	
+
+Listit.XSSDecode = function(s) {
+
+    s = s.replace(/&amp;/g,  "&"); 
+    s = s.replace(/&#39;/g,  "'"); 
+    s = s.replace(/&quot;/g, '"');
+    s = s.replace(/&lt;/g,   "<");
+    s = s.replace(/&gt;/g,   ">");
+    return s;
+}
+
 
 
 
