77e1297951cd36433b666f9a6689e1cac1dad9d1
Pepijn Kenter <titusjan@gmail.com>
2012-03-25 13:57:19 +0000

    Script to minify using the google closure compiler (Works only on my laptop :-)
    git-svn-id: file:///var/svn/programming/listit@269 1fc08600-0457-4e62-b340-16eb51c54387

---
+++ external/minify.sh
@@ -0,0 +1,4 @@
+# Minify using the google closure compiler.
+# https://developers.google.com/closure/compiler/?hl=en-US
+
+java -jar ~/Install/google-closure-compiler/compiler-latest/compiler.jar --js jquery.flot.js --js_output_file=jquery.flot.min.js
