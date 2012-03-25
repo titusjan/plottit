07bb183c2fc26dcfd12ad3ae9ac42195c3d19034
Pepijn Kenter <titusjan@gmail.com>
2012-03-25 14:00:36 +0000

    Comment.
    git-svn-id: file:///var/svn/programming/listit@270 1fc08600-0457-4e62-b340-16eb51c54387

--- external/minify.sh
+++ external/minify.sh
@@ -1,4 +1,5 @@
 # Minify using the google closure compiler.
 # https://developers.google.com/closure/compiler/?hl=en-US
 
+# Assumes the compiler is installed in ~/Install/google-closure-compiler/compiler-latest/
 java -jar ~/Install/google-closure-compiler/compiler-latest/compiler.jar --js jquery.flot.js --js_output_file=jquery.flot.min.js
